554d738dab73714045acebe77ada33f336bfe1ae
unknown <pradeepcr135@gmail.com>
2020-08-14 20:27:50 +0530

    slightly modified

--- demo.py
+++ demo.py
@@ -1,1 +1,6 @@
 print ("this is the code to demonstrate the git basics")
+
+
+git_add = "add files to staging area"
+git_commit = "from staging area it saves to local repo"
+git_push = "from localm repo to remote repo"
